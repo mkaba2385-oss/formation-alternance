504744acd190e0f610544723bf165060ca7be274
Moussa Kaba <mkaba2385@gmail.com>
2026-07-16 14:27:43 +0200

    feat: ajouter la fonction de division

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 def add (a, b) :
     return (a+b)
+
+def division (a, b):
+    if b =0 :
+        print(Erreur: impossible de diviser un nombre par 0 ou par un nombre negatif)
+    else :
+        return (a / b)
