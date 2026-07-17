fa158986e5d57ff64a1625ceb368608ae93af193
Moussa Kaba <mkaba2385@gmail.com>
2026-07-17 14:38:28 +0200

    feat: ajouter la fonction divide()

--- semaine-01/git-exos/calculs.py
+++ semaine-01/git-exos/calculs.py
@@ -7,3 +7,8 @@ def multiply (a,b) :
 
 def subtract (a , b) :
     return ( a - b)
+def divide (a, b) :
+    if b <= 0 :
+        print ("erreur: impossible de diviser un nombre par 0")
+    else :
+        return (a/b) 
